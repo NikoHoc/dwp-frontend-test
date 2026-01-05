a68502b221a82e5aa2326ed1fcb7035340863f6c
Niko Hoc <niko.owen88@gmail.com>
2026-01-05 18:41:31 +0800

    Setup project: tailwind, antd, json-server

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { DatePicker } from 'antd';
+
+function App() {
+
+  return (
+    <>
+      <p className="">test nikolas hai</p>
+      <DatePicker />
+    </>
+  )
+}
+
+export default App
